03816b73b2aeddb880fe8b1d357caf4cf179ab8d
lruiz49 <lruiz1610@gmail.com>
2025-04-28 23:04:21 -0300

    feat: add Task model with associations and timestamps

---
+++ src/database/models/Task.ts
@@ -0,0 +1,58 @@
+import { Table, Column, DataType, Model, ForeignKey, CreatedAt, UpdatedAt, BelongsTo } from 'sequelize-typescript';
+import Goal from './Goal';
+
+
+@Table({
+  timestamps: true,
+  tableName: 'tasks',
+  modelName: 'Task',
+})
+class Task extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.INTEGER,
+    autoIncrement: true,
+  })
+  declare id: number;
+
+  @ForeignKey(() => Goal)
+  @Column({
+    type: DataType.INTEGER,
+  })
+  declare goalid: number;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare name: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare description: string;
+  
+  @Column({
+    type: DataType.STRING,
+  })
+  declare label: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare duedate: string;
+
+  @CreatedAt
+  declare created_at: Date;
+
+  @UpdatedAt
+  declare updated_at: Date;
+
+  @BelongsTo(()=>Goal, {
+    onDelete: 'CASCADE',
+  })
+  declare goal: Goal;
+}
+
+
+
+export default Task
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { deleteAUser, getUsers } from "../controllers/admin.controller";
-import { validateAdminToken } from "../middlewares/validate-token";
 
 const router = Router()
 
