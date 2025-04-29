0ca8a79256976b995ed541c0df35db94c35195ca
fersantisi <fer.santisi@hotmail.com>
2025-04-28 21:04:43 -0300

    feat: implement getProjectsByUserId endpoint and related service fix: update duedate type to use IsDateString decorator refactor: enhance token handling in validate-token middleware

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 const router = Router();
-import { createNewProject, deleteAProject} from '../controllers/project.controller';
+import {
+  createNewProject,
+  deleteAProject,
+  getProjectsByUserId,
+} from '../controllers/project.controller';
 
 router.post('/create', createNewProject);
 router.delete('/delete/:id', deleteAProject);
 router.put('/update', )
+router.get('/list', getProjectsByUserId);
 
 export default router;
