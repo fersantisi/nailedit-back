081c1c00d6e7ab1a080b7073e49e41d023ed29cb
lruiz49 <lruiz1610@gmail.com>
2025-07-28 23:20:07 -0300

    Update dependencies and implement Google login functionality
    - Updated @types/express and @types/node to latest versions.
    - Added body-parser and google-auth-library as dependencies.
    - Implemented googleLogIn function in login.service.ts to handle Google authentication.
    - Created new user if not found during Google login, using a randomly generated password.

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -1,6 +1,10 @@
 import User from '../database/models/User';
 import bcrypt from 'bcrypt';
 import { tokenGenerator } from '../utils/jwt';
+import { OAuth2Client } from "google-auth-library";
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 export const logIn = async (
   username: string,
@@ -15,6 +19,38 @@ export const logIn = async (
   if (validatePassword) {
     return tokenGenerator(user);
   } else {
+    return null;
+  }
+};
+
+export const googleLogIn = async (
+  token: string
+): Promise<{ authToken: string; refreshToken: string } | null> => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    if (!payload || !payload.email) return null;
+
+    let user = await User.findOne({ where: { email: payload.email } });
+
+    if (!user) {
+        const randomPassword = Math.random().toString(36).substring(2,12);
+
+        const hashedPassword = await bcrypt.hash(randomPassword, 10);
+
+        user = await User.create({
+        username: payload.name || payload.email, // Use Google name as username
+        email: payload.email,
+        password: hashedPassword,
+      });
+    }
+    return tokenGenerator(user);
+  } catch (error) {
+    console.error('Error in googleLogIn:', error);
     return null;
   }
 };
